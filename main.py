03843271a3f5fad54646a50bfa0756d0ea999340
Pasindu Sewmuthu <apsewmuthu@gmail.com>
2026-05-31 13:09:45 +0530

    Change function based method to class based method

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def clean_skills(skills):
-    skills_str = ""
-    try:
-        # Convert string representation of list to an actual list
-        skill_list = ast.literal_eval(skills)
-        skills_str = ", ".join(
-            [str(s).lower().replace(" ", "_") for s in skill_list])
-    except:
-        skills_str = skills.lower()
+class JobRecommender:
+    def __init__(self, data_path):
+        self.df = pd.read_csv(data_path)
+        # Clean skills column
+        self.df['clean_skills'] = self.df['job_skill_set'].apply(
+            self.clean_skills)
 
-    return skills_str
+        # Initialize TF-IDF Vectorizer
+        self.tfidf = TfidfVectorizer()
+        self.tfidf_matrix = self.tfidf.fit_transform(self.df['clean_skills'])
